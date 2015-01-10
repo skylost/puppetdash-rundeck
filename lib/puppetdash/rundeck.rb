f0ac493bb1cd1d4d936d346f47e55fc448551317
weezheel <skylostft@laposte.net>
2015-01-10 14:45:53 +0100

    Deposit initialization

---
+++ lib/puppetdash/rundeck.rb
@@ -0,0 +1,7 @@
+require "puppetdash/rundeck/version"
+
+module Puppetdash
+  module Rundeck
+    # Your code goes here...
+  end
+end
---
+++ lib/puppetdash/rundeck/version.rb
@@ -0,0 +1,5 @@
+module Puppetdash
+  module Rundeck
+    VERSION = "0.0.1"
+  end
+end
